45ae3baad665d17cb7d92aa07726006ebaa9a709
Nourhan Ahmed <nourhan222000@gmail.com>
2024-01-10 20:43:22 -0800

    0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["35.175.64.247", "34.202.157.108"]
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_pack():
